82de30c3b4041cb653ef05ca87f90efa522807d5
b <b@b3k.us>
2012-11-17 13:48:56 -0800

    Add license info

--- test/test.c
+++ test/test.c
@@ -1,3 +1,30 @@
+/*
+    libdtw is derived from the UCR Suite.  The original license is below.
+ */
+    
+/***********************************************************************/
+/************************* DISCLAIMER **********************************/
+/***********************************************************************/
+/** This UCR Suite software is copyright protected © 2012 by          **/
+/** Thanawin Rakthanmanon, Bilson Campana, Abdullah Mueen,            **/
+/** Gustavo Batista and Eamonn Keogh.                                 **/
+/**                                                                   **/
+/** Unless stated otherwise, all software is provided free of charge. **/
+/** As well, all software is provided on an "as is" basis without     **/
+/** warranty of any kind, express or implied. Under no circumstances  **/
+/** and under no legal theory, whether in tort, contract,or otherwise,**/
+/** shall Thanawin Rakthanmanon, Bilson Campana, Abdullah Mueen,      **/
+/** Gustavo Batista, or Eamonn Keogh be liable to you or to any other **/
+/** person for any indirect, special, incidental, or consequential    **/
+/** damages of any character including, without limitation, damages   **/
+/** for loss of goodwill, work stoppage, computer failure or          **/
+/** malfunction, or for any and all other damages or losses.          **/
+/**                                                                   **/
+/** If you do not agree with these terms, then you you are advised to **/
+/** not use this software.                                            **/
+/***********************************************************************/
+/***********************************************************************/
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
